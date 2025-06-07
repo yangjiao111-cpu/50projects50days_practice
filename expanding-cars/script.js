88783991abb9154fea4bb5bf67d9cfab353577a3
yangjiao <3273473023@qq.com>
2025-06-07 11:15:45 +0800

    扩展卡片细节优化

--- expanding-cars/script.js
+++ expanding-cars/script.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll('.card-box li')
 cards.forEach((e) => {
     e.addEventListener('click', (e) => {
-        document.querySelector('.active').className = "";
-        e.target.className = "active";
+        document.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
     })
 })
