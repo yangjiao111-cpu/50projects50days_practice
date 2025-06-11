0bea01baf41620407209f17c4ac9165d1ba96864
yangjiao <3273473023@qq.com>
2025-06-11 18:55:50 +0800

    随机选择

---
+++ random-choice-picker/script.js
@@ -0,0 +1,45 @@
+const text = document.querySelector('#text')
+const tokens = document.querySelector('.tokens')
+let tokenArr = null
+text.addEventListener('keyup', (e) => {
+    console.log(e.key);
+
+    if (e.key === 'Enter') {
+        e.target.value = ''
+        if (tokenArr) {
+            random()
+            tokenArr = null
+        }
+        return
+    }
+    tokens.innerHTML = ''
+    tokenArr = e.target.value.split(',')
+    tokenArr.forEach(e => {
+        if (e !== '') {
+            const span = document.createElement('span')
+            span.innerText = e;
+            tokens.appendChild(span)
+        }
+    })
+})
+function random() {
+    const tags = document.querySelectorAll('.tokens span')
+    let count = 20;
+    const id = Math.floor(Math.random() * tags.length)
+    tags[id].classList.add('active')
+    setTimeout(() => {
+        tags[id].classList.remove('active')
+    }, 50)
+    let timerId = setInterval(() => {
+        count--;
+        const id = Math.floor(Math.random() * tags.length)
+        tags[id].classList.add('active')
+        if (count == 0) {
+            clearInterval(timerId)
+            return
+        }
+        setTimeout(() => {
+            tags[id].classList.remove('active')
+        }, 50)
+    }, 100)
+}
